5004b5641e751907aed1e13db5253283850c99da
Samuel Adekoya <damiloladrizzy@yahoo.com>
2019-06-08 17:06:57 +0100

    solved problem one

--- euler.py
+++ euler.py
@@ -5,8 +5,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 #Solution 1:
+values = []
 i = 3
-while i < 10:
+while i < 1000:
     if (i % 5 == 0) | (i % 3 == 0):
-        print(i)
+        values.append(i)
     i += 1
+    
+print(sum(values))
+
+#Answer = 233168
+
